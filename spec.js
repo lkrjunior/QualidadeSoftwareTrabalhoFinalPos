89c2b3aaf9a90caa8bf91e799fcfa5579b9ed360
Luciano Junior Kruse Rocha <luciano.rocha@linx.com.br>
2019-04-08 14:46:06 -0300

    Teste de login inválido

--- spec.js
+++ spec.js
@@ -1,11 +1,30 @@
 describe('Trabalho Final Qualidade de Software', function() {
 	
+	var expectedConditions = protractor.ExpectedConditions;
+	var timeOutExpectedConditions = 5000;
+	
+	var botaoLoginEntrar = $('.last > a');
+	var campoLogin = element(by.name('logar_email'));
+	var campoSenha = element(by.name('logar_senha'));
+	var botaoLogin = $('.bt-login');
+
+	var mensagemLoginInvalido = $('.alert > p');
+	
+
 	beforeEach(function() {
 		browser.waitForAngularEnabled(false);
-		browser.get('http://automationpractice.com');
+		browser.get('https://www.radanesportes.com.br/');
 	});
 	
-	it('Teste 1', function() {
-		browser.sleep(2000);
+	it('Teste do login inválido', function() {
+		botaoLoginEntrar.click();
+		
+		browser.wait(expectedConditions.visibilityOf(campoLogin), timeOutExpectedConditions, "Elemento não encontrado!");
+		campoLogin.sendKeys('teste');
+
+		botaoLogin.click();
+		
+		browser.wait(expectedConditions.visibilityOf(mensagemLoginInvalido), timeOutExpectedConditions, "Mensagem não encontrada!");
+		expect(mensagemLoginInvalido.getText()).toBe('Por favor preencha o e-mail corretamente.');
 	});
 });
